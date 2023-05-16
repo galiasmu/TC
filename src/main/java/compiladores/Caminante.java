30cf752194cf604448218ce3f4adf7169f6ddcdd
Gali Asmuzi <63879587+galiasmu@users.noreply.github.com>
2023-05-16 20:45:21 -0300

    modificando caminantes

--- src/main/java/compiladores/Caminante.java
+++ src/main/java/compiladores/Caminante.java
@@ -3,22 +3,34 @@ package compiladores;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import compiladores.compiladoresParser.AsignacionContext;
+import compiladores.compiladoresParser.ProgramaContext;
 
 public class Caminante extends compiladoresBaseVisitor<String> {
 
+    private Integer asignaciones = 0;
+
     @Override
     public String visitAsignacion(AsignacionContext ctx) {
+        System.out.println("Soy un nodo y tengo " +  ctx.getChildCount()  +  " hijos");
+        System.out.println(" ->     Asignando valor a " + ctx.getChild( 0).getText());
+        //return super.visitAsignacion(ctx);
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitPrograma(ProgramaContext ctx) {
         // TODO Auto-generated method stub
-        System.out.println("SOy un nodo y tengo " + ctx.getChildCount() + "hijos");
-        return super.visitAsignacion(ctx);
+        String ret = super.visitPrograma(ctx);
+        System.out.println("Se realizaron " + asignaciones + " Asignaciones");
+        return ret;
     }
 
     @Override
     public String visitTerminal(TerminalNode node) {
-        // TODO Auto-generated method stub
-        System.out.println("Soy una hoja y tengo el token" + node.getText());
+       // System.out.println("Soy una hoja y tengo el token " + node.getText());
         return super.visitTerminal(node);
     }
     
 }
+
 
